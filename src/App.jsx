4f060fc53a6ca5e225478ae6833e7c1fee3ee67f
mrsurajtripathi <surajtripathi.mtsl@gmail.com>
2026-01-09 14:40:17 +0530

    Event Handler and State

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import TabComponent from './pages/TabComponent';
-import EXAMPLES from './data/data.js';
+import {EXAMPLES} from './data/data.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState('component');
   const handleClick = (name) => {
-    console.log(name);
+    setSelectedTopic(name)
   };
 
   return (
     <>
       <h1>Home</h1>
-      <TabComponent name="Props" onSelect={() => handleClick('Props')} />
+      <TabComponent name="JSX" onSelect={() => handleClick('jsx')} />
       <TabComponent
         name="Components"
-        onSelect={() => handleClick('Components')}
+        onSelect={() => handleClick('components')}
       />
-      <TabComponent name="Pages" onSelect={() => handleClick('Pages')} />
+      <TabComponent name="Props" onSelect={() => handleClick('props')} />
+      <TabComponent name="State" onSelect={() => handleClick('state')} />
       <div className="text" id="tab-content">
-        {selectedTopic}
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <code>{EXAMPLES[selectedTopic].code}</code>
       </div>
     </>
   );
