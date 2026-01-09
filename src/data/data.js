34d36c180aad80651e0413a29dba50f9bf5f9fdd
mrsurajtripathi <surajtripathi.mtsl@gmail.com>
2026-01-09 14:32:53 +0530

    images added

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-import componentsImg from './assets/components.png';
+import componentsImg from './assets/component.png';
 import propsImg from './assets/config.png';
 import jsxImg from './assets/jsx-ui.png';
 import stateImg from './assets/state-mgmt.png';
